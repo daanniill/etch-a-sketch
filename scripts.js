c4179b09780511aa4665e7605131c35fd1dbe8c7
daanniill <daniil.malakhov12@gmail.com>
2025-08-03 12:53:53 -0700

    added functionality to change size of grid in ui. User can now input size of the grid that they want and see it update realtime in the frontend

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ function createGrid(grid_size) {
     let box_num = grid_size * grid_size
     let box_size = 640 / grid_size
     const grid = document.querySelector(".grid")
+    grid.innerHTML = '';
     for (let i = 0; i < box_num; i++) {
         let box = document.createElement("div")
         box.style.width = `${box_size}px`
@@ -9,6 +10,7 @@ function createGrid(grid_size) {
         box.setAttribute("class", "box")
         grid.appendChild(box)
     }
+    hover()
 }
 
 function hover() {
@@ -21,8 +23,16 @@ function hover() {
 }
 
 function changeSize() {
-
+    const change_size = document.getElementById('size_selector');
+    change_size.addEventListener('click', function() {
+        let input = prompt('Enter a size between 2-100', 2)
+        while (input > 100 || input < 2) {
+            input = prompt('Enter a size between 2-100', 2)
+        }
+        createGrid(input);
+    });
 }
 
+
+changeSize()
 createGrid(16)
-hover()
